4c8609a85553c603771112461a1eed95a473b714
OMOTO Kenji <doloopwhile@gmail.com>
2013-02-03 03:07:03 +0900

    Created html2browse.py

---
+++ html2browse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''
+Display html input on browser.
+
+Known issue:
+    This script does not delete temporary file.
+'''
+import sys
+import tempfile
+import fileinput
+import webbrowser
+from argparse import ArgumentParser
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('files', nargs='*', action='store')
+
+    args = parser.parse_args()
+
+    f = tempfile.NamedTemporaryFile('w', prefix='html2browse.',
+            suffix='.html', delete=False)
+
+    for line in fileinput.input(args.files):
+        f.write(line)
+
+    webbrowser.open(f.name)
+
+if __name__ == '__main__':
+    main()
