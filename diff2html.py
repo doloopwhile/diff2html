af36cdd980a69157597155bc1866bb77797a31da
OMOTO Kenji <doloopwhile@gmail.com>
2013-02-03 03:08:22 +0900

    Brushed up diff2html.py

--- diff2html.py
+++ diff2html.py
@@ -1,51 +1,50 @@
 #!/usr/bin/env python3
+from __future__ import print_function, unicode_literals
+import io
 import re
 import fileinput
 import sys
 from argparse import ArgumentParser
+from functools import partial
 
 def quote_html(s):
+    '''Quote html special chars and replace space with nbsp'''
     def repl_quote_html(m):
         tokens = []
+        quote_dict = {
+            ' ': '&nbsp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '&': '&amp;',
+            '"': '&quot;',
+        }
         for c in m.group(0):
-            tokens.append({
-                ' ': '&nbsp;',
-                '<': '&lt;',
-                '>': '&gt;',
-                '&': '&amp;',
-                '"': '&quot;',
-            }[c])
+            tokens.append(quote_dict[c])
         return ''.join(tokens)
     return re.sub('[ &<>"]', repl_quote_html, s)
 
-def main():
-    parser = ArgumentParser()
-    parser.add_argument('--output-file', '-o', action='store')
-    parser.add_argument('files', nargs='*', action='store')
 
-    args = parser.parse_args()
-    if args.output_file:
-        output_file = open(args.output_file, 'w',
-                encoding=sys.getdefaultencoding())
-    else:
-        output_file = sys.stdout
-    def p(output_file=output_file):
-        return lambda *a, **kw: print(file=output_file, *a, **kw)
-    p = p()
+def print_html(print_function, lines, title, encoding):
+    p = print_function
+    q = quote_html
+    p('<?DOCTYPE html?>')
+    p('<html>')
+    p('<head>')
+    p('<meta http-equiv="Content-Type" content="text/html; charset={}">'
+            .format(q(encoding)))
+    if title is not None:
+        p('<title>{}</title>'.format(q(title)))
     p('''
-        <html>
-        <head>
-            <style>
+        <style>
             span.diffcommand { color: teal; }
             span.removed     { color: red; }
             span.inserted    { color: green; }
             span.linenumber  { color: purple; }
-            </style>
-        </head>
+        </style>
     ''')
+    p('</head>')
 
-    for line in fileinput.input(args.files):
-        q = quote_html
+    for line in lines:
         if line.startswith('+++'):
             p(q(line))
         elif line.startswith('---'):
@@ -66,9 +65,31 @@ def main():
             else:
                 p(q(line))
         p('<br />')
-
     p('</body>')
     p('</html>')
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--output-file', '-o', action='store')
+    parser.add_argument('--output-encoding', action='store',
+            default=sys.getdefaultencoding())
+    parser.add_argument('--title', action='store')
+    parser.add_argument('files', nargs='*', action='store')
+
+    args = parser.parse_args()
+    encoding = args.output_encoding
+
+    if args.output_file:
+        output_file = io.open(args.output_file, 'w', encoding=encoding)
+    else:
+        output_file = sys.stdout
+
+    try:
+        print_html(partial(print, file=output_file),
+            fileinput.input(args.files), title=args.title, encoding=encoding)
+    finally:
+        output_file.close()
 
 if __name__ == '__main__':
     main()
